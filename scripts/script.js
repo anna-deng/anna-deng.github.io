67184a6f587ca01b3f55c59b548c80f34365bfc8
anna-deng <annadeng2020@u.northwestern.edu>
2019-12-15 12:38:43 -0800

    total rebrand!

--- scripts/script.js
+++ scripts/script.js
@@ -22,5 +22,5 @@ var sites = [
   function randomSite() {
     var i = parseInt(Math.random() * sites.length);
     location.href = sites[i];
-    window.open(sites[i]);
+    window.open(sites[i], '_blank');
   }
